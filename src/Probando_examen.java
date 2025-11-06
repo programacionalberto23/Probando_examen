a59fdfae56feed7122b1c799f3f8f1b0b79ea6c3
programacionalberto23 <alberto2022trabajo@gmail.com>
2025-11-07 00:27:24 +0100

    Mi primer commit

---
+++ src/Probando_examen.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package randomworldgenerator;
+
+/**
+ *
+ * @author EAG
+ */
+public class RandomWorldGenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        System.out.println("Prueba 1 commit");
+        System.out.println("Prueba 2 pull request");
+    }
+    
+}
